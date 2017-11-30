b9c3c19082966a3c3d1b8bcb700c5aecae09985a
tobiasw225@gmail.com <tobiasw225@gmail.com>
2017-11-30 13:06:39 +0100

    removed tobias testproject

--- tobiastestproject/src/de/uni_leipzig/digital_forensics/testpackage/preprocessing/XMLFileFilter.java
+++
@@ -1,9 +0,0 @@
-package de.uni_leipzig.digital_forensics.testpackage.preprocessing;
-
-import java.io.File;
-
-public class XMLFileFilter {
-	   public boolean accept(File pathname) {
-		      return pathname.getName().toLowerCase().endsWith(".xml");
-		   }
-}
