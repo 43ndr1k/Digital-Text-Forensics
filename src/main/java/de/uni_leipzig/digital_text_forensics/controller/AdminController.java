91c3c332a90112babc05328d8f163bad2cf49f62
Hendrik Sawade <43ndr1k@it-sawade.de>
2018-03-30 18:45:08 +0200

    fix login

--- src/main/java/de/uni_leipzig/digital_text_forensics/controller/AdminController.java
+++ src/main/java/de/uni_leipzig/digital_text_forensics/controller/AdminController.java
@@ -27,12 +27,12 @@ public class AdminController {
 
 	@GetMapping("/login")
 	public String login() {
-		return "/login";
+		return "login";
 	}
 
 	@GetMapping("/403")
 	public String error403() {
-		return "/error/403";
+		return "error/403";
 	}
 
 	/**
